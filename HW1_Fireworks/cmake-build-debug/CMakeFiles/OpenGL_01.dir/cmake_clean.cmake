f5fe55490b4d65059b5af92a6465dee156a02cf3
vladpc <nfuraksdo190@gmail.com>
2019-02-04 17:54:21 -0600

    Figuring out problem

---
+++ HW1_Fireworks/cmake-build-debug/CMakeFiles/OpenGL_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_01.dir/Homework1.cpp.o"
+  "OpenGL_01.pdb"
+  "OpenGL_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
