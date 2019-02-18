ac62e33e78cdb4cf42bb527b7c5f2e90d49d7139
vladpc <nfuraksdo190@gmail.com>
2019-02-17 19:01:23 -0600

    Almost done. There is a bug that is caused by spamming any of the movements. The x,y,z is not updated fast enough

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -1,6 +1,10 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
+//
+// Created by vladpc on 2/17/19.
+//
+
+//#include <math.h>
+//#include <stdio.h>
+//#include <stdlib.h>
 #include <GL/glut.h>
 #include <iostream>
 #include <vector>
@@ -12,6 +16,7 @@ using namespace std;
 GLenum mode = GL_LINE_LOOP;
 int xangle = 10;
 int yangle = 15;
+int zangle = 0;
 float playerX = 0;
 float playerY = 0;
 float playerZ = 0;
@@ -22,7 +27,6 @@ float negativeFixed = -0.6;
 bool fly = true;
 float movementSpeed = 0.2;
 vector<Cube> cubes;
-//bool freeMode = false;
 
 //---------------------------------------
 // Init function for OpenGL
@@ -37,35 +41,24 @@ void init() {
     glEnable(GL_DEPTH_TEST);
     Cube bigCube(0.0, 0.0, 0.0, mode, 1.4);
     cubes.push_back(bigCube);
-
-//    glRotatef(30.0, 1.0, 1.0, 1.0); //this is why the cube is slanted
 }
+
+
+//---------------------------------------
+// Draw the player
+//---------------------------------------
 
 void drawPlayer() {
     glPointSize(10.0);
     glBegin(GL_POINTS);
     glColor3f(1.0, 0.0, 0.0);
-//    if (playerX + movementSpeed + 0.05 > positiveBoundary)
-//        movementSpeed = 0;
-//    else if (playerX - movementSpeed  - 0.055< negativeBoundary)
-//        movementSpeed = 0;
-//    else if (playerY + movementSpeed + 0.055> positiveBoundary)
-//        movementSpeed = 0;
-//    else if (playerY - movementSpeed - 0.055< negativeBoundary)
-//        movementSpeed = 0;
-//    else if (playerZ + movementSpeed + 0.055> positiveBoundary)
-//        movementSpeed = 0;
-//    else if (playerZ - movementSpeed -0.055 < negativeBoundary)
-//        movementSpeed = 0;
-//    else
-//        movementSpeed = 0.2;
-
-
     glVertex3f(playerX, playerY, playerZ);
     glEnd();
 }
 
-
+//---------------------------------------
+// Spawn the cube when the key is pressed
+//---------------------------------------
 void spawnCube() {
     Cube temp(playerX, playerY, playerZ, GL_POLYGON, 0.2);
     cubes.push_back(temp);
@@ -82,6 +75,7 @@ void display() {
     glLoadIdentity();
     glRotatef(xangle, 1.0, 0.0, 0.0);
     glRotatef(yangle, 0.0, 1.0, 0.0);
+    glRotatef(zangle, 0.0, 0.0, 1.0);
     for (Cube c: cubes) {
         c.draw();
     }
@@ -89,44 +83,35 @@ void display() {
     glFlush();
 }
 
-
-void playerBound(){
-    //Prevents the player from leaving the box
-    if (playerX < negativeBoundary) {
-        playerX = negativeFixed;
-    } else if (playerX > positiveBoundary) {
-        playerX = positiveFixed;
-    } else if (playerY > positiveBoundary) {
-        playerY = positiveFixed;
-    } else if (playerY < negativeBoundary) {
-        playerY = negativeFixed;
-    } else if (playerZ > positiveBoundary) {
-        playerZ = positiveFixed;
-    } else if (playerZ < negativeBoundary) {
-        playerZ = negativeFixed;
-    }
+//---------------------------------------
+// Restarts all values to default but keeps the boxes
+//---------------------------------------
+void restart() {
+    zangle = 0;
+    xangle = 10;
+    yangle = 15;
+    playerX = 0;
+    playerY = 0;
+    playerZ = 0;
 }
 
+//---------------------------------------
+// Restarts all values to default and removes all the cubes
+//---------------------------------------
+void fullRestart() {
+    zangle = 0;
+    xangle = 10;
+    yangle = 15;
+    playerX = 0;
+    playerY = 0;
+    playerZ = 0;
+    cubes.erase(cubes.begin() + 1, cubes.end());
+}
 
 //---------------------------------------
 // Keyboard callback for OpenGL
 //---------------------------------------
 void keyboard(unsigned char key, int x, int y) {
-
-    //Free mode disabled for now
-//    if (key == 'q') {
-//        freeMode = true;
-//        cout << "Movement speed set to 0.03" << endl;
-//    } else if (key == 'Q') {
-//        freeMode = false;
-//        cout << "Movement speed set to 0.2" << endl;
-//    }
-
-//    if (freeMode) {
-//        movementSpeed = 0.03;
-//    } else
-//        movementSpeed = 0.2;
-
     //Switch between rotate and fly modes
     if (key == 'F' || key == 'f') {
         fly = true;
@@ -146,56 +131,74 @@ void keyboard(unsigned char key, int x, int y) {
             xangle += 5;
         else if (key == 'Y')
             yangle += 5;
+        else if (key == 'z')
+            zangle -= 5;
+        else if (key == 'Z')
+            zangle += 5;
     }
 
     //Update player position
     if (fly == true) {
-        if (key == 'x')
+        if (key == 'x') {
             playerX -= movementSpeed;
-        else if (key == 'X')
+            if (playerX < negativeBoundary)
+                playerX = negativeFixed;
+        } else if (key == 'X') {
             playerX += movementSpeed;
-        else if (key == 'y')
+            if (playerX > positiveBoundary)
+                playerX = positiveFixed;
+        } else if (key == 'y') {
             playerY -= movementSpeed;
-        else if (key == 'Y')
+            if (playerY < negativeBoundary)
+                playerY = negativeFixed;
+        } else if (key == 'Y') {
             playerY += movementSpeed;
-        else if (key == 'Z')
+            if (playerY > positiveBoundary)
+                playerY = positiveFixed;
+        } else if (key == 'Z') {
             playerZ += movementSpeed;
-        else if (key == 'z')
+            if (playerZ > positiveBoundary)
+                playerZ = positiveFixed;
+        } else if (key == 'z') {
             playerZ -= movementSpeed;
+            if (playerZ < negativeBoundary)
+                playerZ = negativeFixed;
+        }
     }
 
-    playerBound();
-
+    //Spawn and remove boxes
     if (key == '+')
         spawnCube();
     else if (key == '-' && cubes.size() != 1) {
         for (int i = 1; i < cubes.size(); i++) {
             if (cubes[i].isEqual(playerX, playerY, playerZ)) {
                 cout << "Found one!" << endl;
-//                cubes.erase(cubes.begin() + i);
                 cubes[i] = cubes.back();
                 cubes.pop_back();
             }
         }
     }
-//        cubes.pop_back();
 
-    cout << "PlayerX: " << playerX << endl;
-    cout << "PlayerY: " << playerY << endl;
-    cout << "PlayerZ: " << playerZ << endl;
+    //Restart the world
+    if (key == 'd')
+        restart();
+    else if (key == 'D')
+        fullRestart();
 
+//    cout << "PlayerX: " << playerX << endl;
+//    cout << "PlayerY: " << playerY << endl;
+//    cout << "PlayerZ: " << playerZ << endl;
 
     // Redraw objects
     glutPostRedisplay();
 }
-
 
 //---------------------------------------
 // Main program
 //---------------------------------------
 int main(int argc, char *argv[]) {
     glutInit(&argc, argv);
-    glutInitWindowSize(1000, 1000);
+    glutInitWindowSize(1100, 1100);
     glutInitWindowPosition(250, 250);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     glutCreateWindow("Homework 2: Cube");
@@ -204,14 +207,14 @@ int main(int argc, char *argv[]) {
     init();
 
     printf("Keyboard commands:\n");
+    printf("   Currently in fly mode \n");
     printf("   Press F/f - go into fly mode\n");
     printf("   Press R/r - go into rotate mode\n");
     printf("   Fly mode: Press x/X for left/right, y/Y for up/down, z/Z for forward/background \n");
     printf("   Rotate mode: Press x/X, y/Y, z/Z \n");
     printf("   Press + to place box and - to remove box \n");
+    printf("   Press d to restart position and maintain cubes, D to restart position and remove cubes \n");
 //    printf("   Press q/Q to go toggle movement speed 0.03/0.2 \n");
-
-
     glutMainLoop();
     return 0;
 }
