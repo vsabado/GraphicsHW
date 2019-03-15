1430a7bf350faa5488afd862e0c0cba82b82fdb9
vsabado <VladS569041923>
2019-03-15 13:54:42 -0500

    Added a little divider to make it look better

--- Homework4/main.cpp
+++ Homework4/main.cpp
@@ -17,13 +17,13 @@ Coords generateRandomValues() {
 //---------------------------------------
 // Translate x, y, z values
 //---------------------------------------
-
 Coords translateValues(Coords coords, Coords translateBy) {
     Coords result(coords.x + translateBy.x, coords.y + translateBy.y, coords.z + translateBy.z);
     return result;
 }
 
 void performTranslation() {
+    cout << "=======================================    PERFORMING TRANSFORMATION FUNCTION   =========================================================" << endl;
     Coords coords = generateRandomValues();
     Coords translateBy = generateRandomValues();
     cout << "Coordinate values to translate: " << endl;
@@ -35,9 +35,31 @@ void performTranslation() {
     result.printValues();
 }
 
+//---------------------------------------
+// Scale x, y, z values
+//---------------------------------------
+Coords scaleValues(Coords coords, Coords scaleBy) {
+    Coords result(coords.x * scaleBy.x, coords.y * scaleBy.y, coords.z * scaleBy.z);
+    return result;
+}
+
+void performScale() {
+    cout << "=======================================    PERFORMING SCALE FUNCTION   =========================================================" << endl;
+    Coords coords = generateRandomValues();
+    Coords scaleBy = generateRandomValues();
+    cout << "Coordinate values to scale: " << endl;
+    coords.printValues();
+    cout << "Coordinate values to scale by " << endl;
+    scaleBy.printValues();
+    cout << "Resulting scale (x', y', z') : " << endl;
+    Coords result = scaleValues(coords, scaleBy);
+    result.printValues();
+}
+
 
 int main() {
     //Translate operations
     performTranslation();
+    performScale();
     return 0;
 }
